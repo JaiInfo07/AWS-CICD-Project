0b283ec92aded79a922f02091ba75c13ec10e20e
JaiInfo07 <kavivignesh07@gmail.com>
2025-01-25 15:57:04 +0530

    Triggering the Deploy Stage

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,5 @@ docker pull vidocker07/simple-python-apps:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 vidocker07/simple-python-apps:latest
+
+echo "Container Created Successfully!"
