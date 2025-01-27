9dbb9553b0cc6dd1468a4520aec1492ac4893a3f
JaiInfo07 <kavivignesh07@gmail.com>
2025-01-27 13:35:19 +0530

    Updated start_container.sh for Triggering the Build Process in Code Pipeline.

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -7,4 +7,4 @@ docker pull vidocker07/flask-app:latest
 # Run the Docker image as a container
 docker run -d -p 8000:8000 vidocker07/flask-app:latest
 
-echo "Container Created Successfully!"
+echo "Hurray! Container Created Successfully!"
