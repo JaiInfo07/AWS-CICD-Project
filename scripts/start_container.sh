4fc71656cd6a2bba3be7eefe87df2ee5babd36b3
JaiInfo07 <kavivignesh07@gmail.com>
2025-01-27 13:24:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vidocker07/simple-python-apps:latest
+docker pull vidocker07/flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vidocker07/simple-python-apps:latest
+docker run -d -p 5000:5000 vidocker07/flask-app:latest
 
 echo "Container Created Successfully!"
